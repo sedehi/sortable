b3be6fc7402ebd8861d84dfcf92d3a594695f9eb
Navid Sedehi <navid.sedehi@gmail.com>
2015-08-04 21:58:33 +0430

    initial package

---
+++ src/Sedehi/Sortable/Sortable.php
@@ -0,0 +1,49 @@
+<?php namespace Sedehi\Sortable;
+
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Config;
+
+trait Sortable
+{
+
+    public function scopeSortable($query)
+    {
+        if (Request::has('sort') && Request::has('order') && $this->columnExists(Request::get('sort'))) {
+            return $query->orderBy(Request::get('sort'), Request::get('order'));
+        } else {
+            return $query;
+        }
+    }
+
+    public static function link(array $parameters)
+    {
+        if (count($parameters) == 1) {
+            $parameters[1] = ucfirst($parameters[0]);
+        }
+        $col   = $parameters[0];
+        $title = $parameters[1];
+        $icon  = Config::get('sortable.sortable_icon');
+
+        $parameters = [
+            'sort'  => $col,
+            'order' => Request::get('order') === 'asc' ? 'desc' : 'asc'
+        ];
+        $controller = Request::route()->getAction()['controller'];
+        $namespace  = Request::route()->getAction()['namespace'];
+        $controller = str_replace($namespace.'\\', '', $controller);
+
+        $url = action($controller, array_merge(Request::all(), $parameters));
+        
+        return '<a href="'.$url.'"'.'>'.e($title).'</a>'.' '.'<i class="'.$icon.'"></i>';
+    }
+
+    private function columnExists($column)
+    {
+        if (!isset($this->sortable)) {
+            return Schema::hasColumn($this->getTable(), $column);
+        } else {
+            return in_array($column, $this->sortable);
+        }
+    }
+}
---
+++ src/Sedehi/Sortable/SortableServiceProvider.php
@@ -0,0 +1,52 @@
+<?php namespace Sedehi\Sortable;
+
+use Illuminate\Support\ServiceProvider;
+
+class SortableServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+
+
+    public function boot()
+    {
+        $this->publishes([__DIR__.'/../../config/sortable.php' => config_path('sortable.php')]);
+
+        $blade = $this->app['view']->getEngineResolver()->resolve('blade')->getCompiler();
+        $blade->extend(function ($view, $compiler) {
+            $pattern = $compiler->createMatcher('sortlink');
+            $replace = '<?php echo \Sedehi\Sortable\Sortable::link(array $2);?>';
+
+            return preg_replace($pattern, $replace, $view);
+        });
+    }
+
+
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__.'/../../config/sortable.php', 'sortable');
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
+
+
+}
